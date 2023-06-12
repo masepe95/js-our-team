b989037b3fc85bf38dab856f7d332bc8e6173dad
masepe95 <marcosepe10@gmail.com>
2023-06-12 22:33:02 +0200

    created array

--- js/script.js
+++ js/script.js
@@ -1,1 +1,58 @@
 console.log('JS OK') 
+
+const membri = [
+	{
+		nome: "Wayne",
+		cognome: "Barnett",
+		grado: "Founder & CEO",
+		immagine: "../img/wayne-barnett-founder-ceo.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+	{
+		nome: "Angela",
+		cognome: "Caroll",
+		grado: "Chief Editor",
+		immagine: "../img/angela-caroll-chief-editor.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+	{
+		nome: "Walter",
+		cognome: "Gordon",
+		grado: "Office Manager",
+		immagine: "../img/walter-gordon-office-manager.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+	{
+		nome: "Angela",
+		cognome: "Lopez",
+		grado: "Social Media Manager",
+		immagine: "../img/angela-lopez-social-media-manager.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+	{
+		nome: "Scott",
+		cognome: "Estrada",
+		grado: "Developer",
+		immagine: "../img/scott-estrada-developer.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+	{
+		nome: "Barbara",
+		cognome: "Ramos",
+		grado: "Graphic Designer",
+		immagine: "../img/barbara-ramos-graphic-designer.jpg",
+		fullName: function () {
+			return this.nome + " " + this.cognome;
+		},
+	},
+];
